21177b2c56e8c8cf476a039a529ec1f0be828736
Steve Cherry <stevenccherry@gmail.com>
2020-08-26 15:07:02 +0000

    Add minification

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,25 @@
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const htmlMinifier = require('html-minifier');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  eleventyConfig.addTransform('html-minifier', (content, outputPath) => {
+    if (
+      process.env.ELEVENTY_ENV === 'production' &&
+      outputPath.endsWith('.html')
+    ) {
+      const minified = htmlMinifier.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dataTemplateEngine: 'njk',
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,15 @@
+const postcssCsso = require('postcss-csso');
 const postcssImport = require('postcss-import');
 const postcssNormalize = require('postcss-normalize');
 const postcssPresetEnv = require('postcss-preset-env');
 const tailwind = require('tailwindcss');
 
 module.exports = {
-  plugins: [postcssImport, postcssNormalize, tailwind, postcssPresetEnv],
+  plugins: [
+    postcssImport,
+    postcssNormalize,
+    tailwind,
+    postcssPresetEnv,
+    ...(process.env.NODE_ENV === 'production' ? [postcssCsso] : []),
+  ],
 };
